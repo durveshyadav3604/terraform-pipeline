7f91d4c51e4bbb64ace6d1c1fdf9ebff2c9e1aa5
durveshyadav3604 <durvesh.yadav3604@gmail.com>
2026-01-21 00:08:52 +0530

    Restore output definitions for webapp instances

--- dev/outputs.tf
+++ dev/outputs.tf
@@ -5,7 +5,3 @@ output "fetched_info_from_webapp1" {
 output "fetched_info_from_webapp2" {
   value = format("%s%s","ssh -i mykey ec2-user@",aws_instance.webapp2.public_ip)
 }
-
-output "alb_dns_name" {
-  value = aws_lb.alb.dns_name
-}
